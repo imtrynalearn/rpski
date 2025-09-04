815b6c7456d3a26d1acdef044bb0ecd05d0f4636
imtrynalearn <123456+imtrynalearn@users.noreply.github.com>
2025-09-04 20:01:47 +0800

    your message

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,5 @@ import Stripe from "stripe";
 import { env } from "@/lib/env";
 
 export const stripe = env.STRIPE_SECRET_KEY
-  ? new Stripe(env.STRIPE_SECRET_KEY, { apiVersion: "2024-06-20" })
+  ? new Stripe(env.STRIPE_SECRET_KEY, { apiVersion: "2023-10-16" })
   : (null as unknown as Stripe);
-
